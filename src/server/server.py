0eafaf1a3c8d1dbe1f5f83f3bc714db7ec5a8f17
victor <victorkrook96@gmail.com>
2021-02-17 12:30:37 +0100

    Enhanced server with its own class.

--- src/server/server.py
+++ src/server/server.py
@@ -1,30 +1,81 @@
 import asyncio
 
-from api import protocol
+from subscription import Subscription
+from publication import Publication
+from subscription_container import SubscriptionContainer
 from server_request import Request
 from utils.config import Config
 from utils.log import Log
 
 
-async def request_handler(reader: asyncio.StreamReader,
-                          writer: asyncio.StreamWriter) -> None:
-    req = Request(reader, writer)
-    await req.respond()
-    #packet = await protocol.read_packet(reader)
+MAKE_PING_DELAY = 1     # In seconds.
 
 
+class Server:
 
-async def main() -> None:
-    config = Config.get()
-    server = await asyncio.start_server(request_handler,
-                                        config['server']['ip'],
-                                        config['server']['port'])
-    Log.info(f'Server started at {server.sockets[0].getsockname()}')
+    def __init__(self):
+        self._sub_container = SubscriptionContainer()
+        self._ping_pong_tasks = asyncio.PriorityQueue()
 
-    async with server:
-        await server.serve_forever()
+    async def _make_pings(self):
+        """ Task that sends pings to all subscriptions in the queue.
+            All subs in this queue have timed out.
+        """
+        while True:
+            await self._check_timeouts()
+            while not self._ping_pong_tasks.empty():
+                ping_task = await self._ping_pong_tasks.get()
+                await ping_task.ping()
+
+            # Go idle so other tasks can run.
+            await asyncio.sleep(MAKE_PING_DELAY)
+
+    async def _check_timeouts(self):
+        """ Checks all the subscribers to see if their timer has timed out
+            and puts all the timed out ones in the priority queue.
+        """
+        subs = self._sub_container.get_all_subs()
+        timed_out_subs = filter(lambda sub: sub.timed_out(), subs)
+        for sub in timed_out_subs:
+            self._ping_pong_tasks.put_nowait(sub)
+
+    async def _make_publications(self, publication: Publication) -> None:
+        """ Sends the publication to all the subscribers of the topic. """
+        subs = self._sub_container.get_subs(publication.topic)
+        for sub in subs:
+            sub.new_data(publication.message)
+
+    async def _add_subscription(self, subscription: Subscription) -> None:
+        self._sub_container.add_sub(subscription)
+
+    async def _request_handler(self,
+                               reader: asyncio.StreamReader,
+                               writer: asyncio.StreamWriter) -> None:
+        """ Handles a TCP request. """
+        request = Request(reader, writer)
+        result = await request.respond()
+
+        if type(result) is Subscription:
+            self._sub_container.add_sub(result)
+
+        elif type(result) is Publication:
+            self._make_publications(result)
+
+        elif type(result) is None:
+            Log.debug('Result is None! ')
+
+    async def start(self) -> None:
+        """ Starts the server. This method runs forever. """
+        config = Config.get()
+        server = await asyncio.start_server(self._request_handler,
+                                            config['server']['ip'],
+                                            config['server']['port'])
+        Log.info(f'Server started at {server.sockets[0].getsockname()}')
+
+        async with server:
+            await server.serve_forever()
 
 
 if __name__ == '__main__':
     Log.init()
-    asyncio.run(main())
+    asyncio.run(Server().start())
