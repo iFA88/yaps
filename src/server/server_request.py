7d2c21e33bc8858c9638bd135051950b6c802984
victor <victorkrook96@gmail.com>
2021-02-17 12:31:21 +0100

    Refactored state away from this class, to Subscription

--- src/server/server_request.py
+++ src/server/server_request.py
@@ -1,15 +1,10 @@
 import asyncio
 
+from subscription import Subscription
+from publication import Publication
 from api import protocol
 from api.packet import Packet
 from utils.log import Log
-
-
-class State:
-    START = 0
-    PING_PONG = 1
-    PING_PONG_1_MISS = 2
-    PING_PONG_2_MISS = 2
 
 
 class Request:
@@ -18,46 +13,44 @@ class Request:
                  writer: asyncio.StreamWriter):
         self._reader = reader
         self._writer = writer
-        self._state = State.START
 
     async def respond(self):
         packet = await protocol.read_packet(self._reader)
 
-        if self._state == State.START:
-            if packet.cmd == protocol.Commands.PUBLISH:
-                await self._handle_publish()
-            elif packet.cmd == protocol.Commands.SUBSCRIBE:
-                await self._handle_subscribe()
-            else:
-                await self._handle_wrong_cmd()
-        elif self._state == State.PING_PONG:
-            pass
-        elif self._state == State.PING_PONG_1_MISS:
-            pass
-        elif self._state == State.PING_PONG_2_MISS:
-            pass
+        if packet.cmd == protocol.Commands.PUBLISH:
+            await self._handle_publish()
+        elif packet.cmd == protocol.Commands.SUBSCRIBE:
+            await self._handle_subscribe()
+        else:
+            await self._handle_wrong_cmd()
 
-    async def _handle_publish(self) -> str:
+    async def _handle_publish(self) -> Publication:
         Log.debug('[Server] Pub')
         # Publish ACK
         await protocol.send_packet(self._writer, protocol.Commands.PUBLISH_ACK)
 
         # Publish 'message' to 'topic'
         packet = await protocol.read_packet(self._reader)
-
-        if not await self._cmd_ok(packet, protocol.Commands.PUBLISH):
+        if not await protocol.cmd_ok(packet, protocol.Commands.PUBLISH,
+                                     self._writer):
             return
 
-        # TODO: Ensure publish is OK?
         data = packet.data.decode('utf-8')
+
+        # Ensure publish is OK according to the format required.
+        if not protocol.publish_ok(data):
+            Log.debug(f'[Server] Pub -> Publish "{data}" is incorrect format.')
+            return None
 
         Log.debug(f'[Server] Pub -> {data}')
 
         # Publish OK
         await protocol.send_packet(self._writer, protocol.Commands.PUBLISH_OK)
-        return data
 
-    async def _handle_subscribe(self) -> str:
+        topic, message = protocol.get_topic_and_msg(data)
+        return Publication(topic, message)
+
+    async def _handle_subscribe(self) -> Subscription:
         Log.debug('[Server] Sub')
 
         # Subscribe ACK
@@ -66,37 +59,25 @@ class Request:
 
         # Subscribe 'topic'
         packet = await protocol.read_packet(self._reader)
+        if not await protocol.cmd_ok(packet, protocol.Commands.SUBSCRIBE,
+                                     self._writer):
+            return None
 
-        if not await self._cmd_ok(packet, protocol.Commands.SUBSCRIBE):
-            return
+        topic = packet.data.decode('utf-8')
 
-        # TODO: Ensure subscription is OK?
-        data = packet.data.decode('utf-8')
+        # Ensure topic is OK according to the format required.
+        if not protocol.topic_ok(topic):
+            Log.debug(f'[Server] Sub -> Topic "{topic}" is incorrect format.')
+            return None
 
-        Log.debug(f'[Server] Sub -> {data}')
+        Log.debug(f'[Server] Sub -> Topic: "{topic}"')
 
-        # Publish OK
+        # Subscribe OK
         await protocol.send_packet(self._writer,
                                    protocol.Commands.SUBSCRIBE_OK)
-        # Change state and return the data
-        self._state = State.PING_PONG
-        return data
+
+        return Subscription(topic, self._reader, self._writer)
 
     async def _handle_wrong_cmd(self):
         Log.debug('[Server] Wrong cmd')
 
-    async def _cmd_ok(self, packet: Packet, cmd: int) -> bool:
-        """
-            Returns true if command is okay, and sends a BAD_CMD packet
-            if the command is not okay.
-        """
-        ok = True
-        if packet is None:
-            Log.err('Failed to read packet!')
-            ok = False
-        elif packet.cmd != cmd:
-            Log.err('Packet command incorrect!')
-            await protocol.send_packet(self._writer, protocol.Commands.BAD_CMD)
-            ok = False
-
-        return ok
