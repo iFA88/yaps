217c3d5f513ed00001daf2e6b1cc0f22166f2926
victor <victorkrook96@gmail.com>
2021-02-18 17:12:51 +0100

    refactored into easier use

--- src/server/server.py
+++ src/server/server.py
@@ -3,7 +3,7 @@ import asyncio
 from subscription import Subscription
 from publication import Publication
 from subscription_container import SubscriptionContainer
-from server_request import Request
+from server_request import Request, RequestResult
 from utils.config import Config
 from utils.log import Log
 
@@ -13,9 +13,12 @@ MAKE_PING_DELAY = 1     # In seconds.
 
 class Server:
 
-    def __init__(self):
-        self._sub_container = SubscriptionContainer()
+    def __init__(self, ip: str, port: int):
+        self._subscriptions = SubscriptionContainer()
         self._ping_pong_tasks = asyncio.PriorityQueue()
+
+        self._ip = ip
+        self._port = port
 
     async def _make_pings(self):
         """ Task that sends pings to all subscriptions in the queue.
@@ -24,8 +27,16 @@ class Server:
         while True:
             await self._check_timeouts()
             while not self._ping_pong_tasks.empty():
-                ping_task = await self._ping_pong_tasks.get()
-                await ping_task.ping()
+                try:
+                    subscriber = await self._ping_pong_tasks.get()
+                    await subscriber.ping()
+                except (ConnectionResetError, BrokenPipeError):
+                    Log.err(f'Connection unexpectedly closed {subscriber}')
+                    self._delete_subscription(subscriber)
+                finally:
+                    # We want to remove the task from the queue regardless
+                    # if it fails or completes.
+                    self._ping_pong_tasks.task_done()
 
             # Go idle so other tasks can run.
             await asyncio.sleep(MAKE_PING_DELAY)
@@ -34,48 +45,56 @@ class Server:
         """ Checks all the subscribers to see if their timer has timed out
             and puts all the timed out ones in the priority queue.
         """
-        subs = self._sub_container.get_all_subs()
+        subs = self._subscriptions.get_all()
         timed_out_subs = filter(lambda sub: sub.timed_out(), subs)
         for sub in timed_out_subs:
             self._ping_pong_tasks.put_nowait(sub)
 
     async def _make_publications(self, publication: Publication) -> None:
         """ Sends the publication to all the subscribers of the topic. """
-        subs = self._sub_container.get_subs(publication.topic)
-        for sub in subs:
-            sub.new_data(publication.message)
+        subs = self._subscriptions.get(publication.topic)
+        for sub in subs.copy():
+            pub_ok = await sub.new_data(publication.message)
+            if not pub_ok:
+                self._delete_subscription(sub)
+
+    def _delete_subscription(self, subscription: Subscription) -> None:
+        self._subscriptions.delete(subscription)
 
     async def _add_subscription(self, subscription: Subscription) -> None:
-        self._sub_container.add_sub(subscription)
+        self._subscriptions.add(subscription)
+        Log.debug(f'Total subscribers: {self._subscriptions.get_all()}')
+        await subscription.start_idle()
 
     async def _request_handler(self,
                                reader: asyncio.StreamReader,
                                writer: asyncio.StreamWriter) -> None:
         """ Handles a TCP request. """
         request = Request(reader, writer)
-        result = await request.respond()
+        result: RequestResult = await request.respond()
 
-        if type(result) is Subscription:
-            self._sub_container.add_sub(result)
+        if result == Subscription:
+            await self._add_subscription(result.data)
 
-        elif type(result) is Publication:
-            self._make_publications(result)
+        elif result == Publication:
+            await self._make_publications(result.data)
 
-        elif type(result) is None:
-            Log.debug('Result is None! ')
+        elif result is None:
+            Log.debug('ALERT: Result is None! ')
 
     async def start(self) -> None:
         """ Starts the server. This method runs forever. """
-        config = Config.get()
         server = await asyncio.start_server(self._request_handler,
-                                            config['server']['ip'],
-                                            config['server']['port'])
+                                            self._ip, self._port)
         Log.info(f'Server started at {server.sockets[0].getsockname()}')
 
         async with server:
-            await server.serve_forever()
+            await asyncio.gather(self._make_pings(),
+                                 server.serve_forever())
 
 
 if __name__ == '__main__':
     Log.init()
-    asyncio.run(Server().start())
+    server = Server(Config.get()['server']['ip'],
+                    Config.get()['server']['port'])
+    asyncio.run(server.start())
