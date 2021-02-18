5967e3e1d8d63ecf8846dfd24e561489becd46cd
victor <victorkrook96@gmail.com>
2021-02-18 17:12:36 +0100

    added repr

--- src/server/publication.py
+++ src/server/publication.py
@@ -3,3 +3,6 @@ class Publication:
     def __init__(self, topic: str, message: str):
         self.topic = topic
         self.message = message
+
+    def __repr__(self):
+        return f'{self.topic}: {self.message}'
