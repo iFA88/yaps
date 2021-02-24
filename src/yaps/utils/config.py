a419cc55c8dfb827139fa25d9ebbcfe221e2c76c
victor <victorkrook96@gmail.com>
2021-02-24 18:01:38 +0100

    Added method to save new default config

--- src/yaps/utils/config.py
+++ src/yaps/utils/config.py
@@ -2,14 +2,12 @@ import json
 import os
 
 from yaps.utils.log import Log
-
+from . import BASE_PATH
 
 __all__ = ['Config']
 
 
-CONFIG_NAME = 'config.json'
-BASE_PATH = os.path.dirname(os.path.abspath(__file__))
-CONFIG_PATH = os.path.join(BASE_PATH, CONFIG_NAME)
+CONFIG_PATH = os.path.join(BASE_PATH, 'config.json')
 
 
 class Config:
@@ -31,19 +29,30 @@ class Config:
     def get() -> dict:
         """
             Returns the configurations.
-            If can't find the configuration file, default parameters are used.
+            If can't find the configuration file, default parameters are used
+            and a new one is created on disk.
         """
+        success = False
 
         # Only need to read the config once (assumes no live-changes).
         if Config._config is None:
             try:
                 with open(CONFIG_PATH, 'rb') as f:
                     Config._config = json.load(f)
+                    success = True
             except FileNotFoundError:
                 Log.err(f'Failed to find config at {CONFIG_PATH}')
-                Config._config = Config.__default
             except json.decoder.JSONDecodeError as e:
                 Log.err(f'Failed to parse JSON {e}')
+
+            if not success:
                 Config._config = Config.__default
+                Config._save_config(Config._config)
 
         return Config._config
+
+    @staticmethod
+    def _save_config(config: dict) -> None:
+        Log.info(f'Saving configuration file at {CONFIG_PATH}')
+        with open(CONFIG_PATH, 'w') as f:
+            json.dump(config, f)
