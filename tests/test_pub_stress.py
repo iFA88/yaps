971ec3574bbebf2d33e8194096e4a4b4045f1711
victor <victorkrook96@gmail.com>
2021-02-26 19:05:08 +0100

    Added small stress test

---
+++ tests/test_pub_stress.py
@@ -0,0 +1,23 @@
+import unittest
+
+from yaps.client.client import Client
+from yaps.utils.config import Config
+from yaps.utils.log import Log
+
+
+class TestStress(unittest.IsolatedAsyncioTestCase):
+
+    def setUp(self):
+        self.ip = Config.get()['client']['ip']
+        self.port = Config.get()['client']['port']
+        self.client = Client(self.ip, self.port)
+        Log.disable()
+
+    async def test_topics_formats(self):
+        ROUNDS = 100
+        for i in range(ROUNDS):
+            await self.client.publish('test', f'Msg: {i}')
+
+
+if __name__ == '__main__':
+    unittest.main()
